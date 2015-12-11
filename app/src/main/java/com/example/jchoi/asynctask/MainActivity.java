7dcfe54237cd2b4a5414c048135148ae4105856c
2016jchoi <janechoi2020@gmail.com>
2015-12-11 10:55:32 -0500

    Final

--- app/src/main/java/com/example/jchoi/asynctask/MainActivity.java
+++ app/src/main/java/com/example/jchoi/asynctask/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jchoi.asynctask;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -14,6 +15,16 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
     public ProgressBar bar = null;
@@ -33,37 +44,66 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
         Button button = (Button) findViewById(R.id.progress);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 DownloadFilesTask dsk = new DownloadFilesTask();
-                dsk.execute(1000,5);
+                dsk.execute("http://shakespeare.mit.edu/hamlet/full.html");
             }
         });
+
         bar = (ProgressBar) findViewById(R.id.progressBar);
     }
 
-    private class DownloadFilesTask extends AsyncTask<Integer, Integer, Integer> {
-        protected Integer doInBackground(Integer... n) {
-            for (int i = 0; i < 1001; i=i+5) {
-                try {
-                    Thread.sleep(5);
-                    publishProgress(i);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+    private class DownloadFilesTask extends AsyncTask<String, Integer, String> {
+
+        protected String doInBackground(String... n) {
+            StringBuilder sb = new StringBuilder();
+
+            try {
+                URL hamlet = new URL(n[0]);
+                URLConnection connection = hamlet.openConnection();
+                connection.connect();
+//                int fileLength = connection.getContentLength();
+//                Log.i("The length of the file is ", fileLength+".");
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    sb.append(inputLine+"\n");
+                    Log.i("Line ", inputLine);
+//                    total = sb.length();
+//                    if (fileLength > 0)
+//                        publishProgress((int)((total*100)/fileLength));
                 }
+                in.close();
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
             }
-            return n[0];
+            return sb.toString();
         }
         protected void onProgressUpdate(Integer... progress) {
-            bar.setProgress(progress[0]);
+           // bar.setProgress(progress[0]);
         }
-        protected void onPostExecute(Integer result) {
+        protected void onPostExecute(String sb) {
             Context context = getApplicationContext();
-            CharSequence text = "Done!";
+            CharSequence text = "Download Complete!";
             int duration = Toast.LENGTH_SHORT;
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
+
+            //try{
+             //   File f = new File("/users/2016jchoi/output.txt");
+            //    FileWriter fw = new FileWriter(f);
+            //    BufferedWriter bw = new BufferedWriter(fw);
+            //    bw.write(sb);
+            //    bw.close();
+           // }
+            //catch (IOException iox) {
+            //    iox.printStackTrace();
+            //}
+            //Log.i("The document is: ", sb);
         }
     }
 
